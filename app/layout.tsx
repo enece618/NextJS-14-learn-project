cd37579adc2bad5e15d9388111a404f0489bebd3
Fernando Nevarez <49252590+enece618@users.noreply.github.com>
2024-03-19 14:55:49 -0600

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,16 @@
+import { Metadata } from 'next';
+import '@/app/ui/global.css';
+import { montserrat } from './ui/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer>
+          Hecho por la gente de vercel
+        </footer>
+      </body>
     </html>
   );
 }
